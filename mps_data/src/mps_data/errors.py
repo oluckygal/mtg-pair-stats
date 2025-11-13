05cb3863035374653fbfa014a5838423d2f3e95f
oluckygal <104176652+oluckygal@users.noreply.github.com>
2025-11-13 12:54:58 -0500

    Handle attempting to load non-csv data

--- mps_data/src/mps_data/errors.py
+++ mps_data/src/mps_data/errors.py
@@ -1,7 +1,11 @@
-class FileNotFoundError(Exception):
-    _MESSAGE_TEMPLATE = "No file found at \"{path}\""
+class PathError(Exception):
+    _MESSAGE_TEMPLATE='{path}'
     def __init__(self, path):
-        self.msg = self._MESSAGE_TEMPLATE.format(path=path)
-        super().__init__(path)
-    def __str__(self):
-        return self.msg
+        message = self._MESSAGE_TEMPLATE.format(path=path)
+        super().__init__(message)
+
+class FileNotFoundError(PathError):
+    _MESSAGE_TEMPLATE = 'No file found at "{path}"'
+
+class NonCsvDataError(PathError):
+    _MESSAGE_TEMPLATE = 'File at "{path}" doesn\'t appear to contain 17lands game data'
--- mps_data/tests/test_mps_data.py
+++ mps_data/tests/test_mps_data.py
@@ -5,6 +5,13 @@ def test_load_game_data():
     assert(isinstance(game_data, mps.GameData))
 
 def test_load_no_file():
+    error_message = 'No file found at "./tests/test_data/no-file.csv"'
     with pytest.raises(mps.errors.FileNotFoundError) as exception_info:
         mps.load_data.from_file('./tests/test_data/no-file.csv')
-    assert(str(exception_info.value) == "No file found at \"./tests/test_data/no-file.csv\"")
+    assert(str(exception_info.value) == error_message)
+
+def test_load_irrelevant_file():
+    error_message = 'File at "./tests/test_data/jabberwocky.txt" doesn\'t appear to contain 17lands game data'
+    with pytest.raises(mps.errors.NonCsvDataError) as exception_info:
+        mps.load_data.from_file('./tests/test_data/jabberwocky.txt')
+    assert(str(exception_info.value) == error_message)
