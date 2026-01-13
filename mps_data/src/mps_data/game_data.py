a5318c848bf26586ba54291688233530b3d0e0e6
oluckygal <104176652+oluckygal@users.noreply.github.com>
2026-01-13 18:38:09 -0500

    Add getter for card names

--- mps_data/src/mps_data/game_data.py
+++ mps_data/src/mps_data/game_data.py
@@ -2,16 +2,26 @@ import pandas as pd
 from .errors import MandatoryColumnsMissingError
 class GameData:
     _MANDATORY_COLUMNS_SET = set(["expansion","event_type","won"])
+    _EVER_IN_HAND_PREFIX = "ever_in_hand_"
     def __init__(self, data: pd.DataFrame):
         self._validate_data(data)
         self._data = data.copy(deep=True)
 
-    def _validate_data(self, data: pd.DataFrame):
+    def _validate_data(self, data: pd.DataFrame) -> None:
         missing_columns = self._MANDATORY_COLUMNS_SET.difference(data.columns)
         if len(missing_columns) > 0:
             raise MandatoryColumnsMissingError(missing_columns)
 
-    def to_dataframe(self):
+    def _generate_card_names(self) -> list[str]:
+        return self._data.filter(regex="^" + self._IN_DECK_PREFIX).columns.map(lambda label: label.split("_")[-1]).to_list()
+    
+    def get_card_names(self) -> list[str]:
+        try:
+            return self._card_names.copy()
+        except AttributeError:
+            self._card_names = self._generate_card_names()
+            return self._card_names.copy()
+    def as_dataframe(self) -> pd.DataFrame:
         return self._data.copy(deep=True)
     
     # def filter_games_by_card(self) -> GameData:
--- mps_data/tests/test_game_data.py
+++ mps_data/tests/test_game_data.py
@@ -2,14 +2,31 @@ import mps_data as mps
 import pandas as pd
 import pytest
 
-def test_to_dataframe_produces_equivalent_dataframe():
+def test_as_dataframe_produces_equivalent_dataframe():
     test_data = {"expansion": ["EOE"], "event_type": ["sealed"], "won": [True]}
     test_dataframe = pd.DataFrame(data=test_data)
     game_data = mps.GameData(test_dataframe)
-    game_data_dataframe = game_data.to_dataframe()
+    game_data_dataframe = game_data.as_dataframe()
     assert(test_dataframe.equals(game_data_dataframe))
     game_data_dataframe.loc[0, 0] = "ECL"
     assert(not test_dataframe.equals(game_data_dataframe))
+
+def test_get_card_names():
+    game_data = mps.load_data.from_file("./tests/test_data/test_game_data.csv")
+    #test caching
+    for i in range(2):
+        card_names = game_data.get_card_names()
+        assert(card_names.count("Reach Through Mists") == 1)
+        assert(card_names.count("Inspiration") == 1)
+        assert(card_names.count("Jace's Ingenuity") == 1)
+        assert(card_names.count("Opportunity") == 1)
+        assert(card_names.count("Totally Lost") == 0)
+    test_data = {"expansion": ["EOE"], "event_type": ["sealed"], "won": [True]}
+    test_dataframe = pd.DataFrame(data=test_data)
+    game_data = mps.GameData(test_dataframe)
+    assert(len(game_data.get_card_names()) == 0)
+
+
     
 # def test_get_improvement_when_drawn_game_count():
 #     game_data = mps.load_data.from_file('./tests/test_data/test_game_data.csv')
