7b4885995a5d332d1f5ffcedb3d082f15d560d20
oluckygal <104176652+oluckygal@users.noreply.github.com>
2026-06-15 21:39:52 -0400

    Add both_drawn_win_data converion to dictionary

--- mps_data/tests/test_both_drawn_win_data.py
+++ mps_data/tests/test_both_drawn_win_data.py
@@ -23,7 +23,7 @@ def test_create_both_drawn_win_data():
     assert(card2_drawn_data["games"].iloc[0] == 4)
     assert(card2_drawn_data["winrate"].iloc[0] == 0.25)
 
-    both_drawn_data = reach_insp_data[~reach_insp_data["card1_drawn"] & ~reach_insp_data["card2_drawn"]]
+    both_drawn_data = reach_insp_data[reach_insp_data["card1_drawn"] & reach_insp_data["card2_drawn"]]
     assert(len(both_drawn_data.index == 1))
     assert(both_drawn_data["games"].iloc[0] == 2)
     assert(both_drawn_data["winrate"].iloc[0] == 0.5)
@@ -40,3 +40,14 @@ def test_create_both_drawn_win_data():
     assert(len(card2_drawn_data.index) == 1)
     assert(card2_drawn_data["games"].iloc[0] == 2)
     assert(card2_drawn_data["winrate"].iloc[0] == 0.5)
+
+def test_as_dict():
+    game_data = mps.load_data.from_file("./tests/test_data/test_both_drawn_win_data.csv")
+    both_drawn_win_data = mps.data_processing.create_both_drawn_win_data(game_data)
+    both_drawn_win_dict = both_drawn_win_data.as_dict()
+    reach_insp_dict = both_drawn_win_dict["Reach Through Mists"]["Inspiration"]
+    assert(reach_insp_dict["Both In Deck Games"] == 10)
+    assert(reach_insp_dict["Neither Drawn Winrate"] == 0.5)
+    assert(reach_insp_dict["First Drawn Winrate"] == 0.5)
+    assert(reach_insp_dict["Second Drawn Winrate"] == 0.25)
+    assert(reach_insp_dict["Both Drawn Winrate"] == 0.5)
---
+++ mps_data/tests/test_data_processing.py
@@ -0,0 +1,4 @@
+# import mps_data as mps
+# import pandas as pd
+# import pytest
+
