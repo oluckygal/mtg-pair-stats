703606491b051c99bce19003aaef30a43ed07cc5
oluckygal <104176652+oluckygal@users.noreply.github.com>
2026-06-15 21:48:59 -0400

    Add ability to convert both_drawn_win_data to a dict

--- mps_data/src/mps_data/data_processing/both_drawn_win_data/both_drawn_win_data.py
+++ mps_data/src/mps_data/data_processing/both_drawn_win_data/both_drawn_win_data.py
@@ -15,12 +15,15 @@ class BothDrawnWinData:
                 card_pair_data = self._generate_win_data_for_card_pair(cards[card1], cards[card2], in_hand_data)
                 for card_pair_dataframe in card_pair_data:
                     both_drawn_data_collector.append(card_pair_dataframe)
+            print(card1)
         self._data = pd.concat(both_drawn_data_collector, axis=0, ignore_index=True)
 
     def _move_card_names_to_data(self, data: pd.DataFrame, card1: str, card2: str) -> pd.DataFrame:
-        card_stats = data.rename(columns={"ever_in_hand_" + card1 : "card1_drawn", "ever_in_hand_" + card2 : "card2_drawn"})
-        card_stats["card1"] = card1
-        card_stats["card2"] = card2
+        card_stats = data.rename(
+            columns={GameData.EVER_IN_HAND_PREFIX + card1 : self._CARD1_DRAWN_COLUMN_INDEX, 
+                     GameData.EVER_IN_HAND_PREFIX + card2 : self._CARD2_DRAWN_COLUMN_INDEX})
+        card_stats[self._CARD1_INDEX] = card1
+        card_stats[self._CARD2_INDEX] = card2
         return card_stats
              
 
@@ -35,10 +38,35 @@ class BothDrawnWinData:
             card1_in_hand_column, 
             card2_in_hand_column])
         card_pair_group_by = card_pair_filtered_data.groupby(by=[card1_in_hand_column, card2_in_hand_column])
-        card_pair_stats = card_pair_group_by.aggregate(games=pd.NamedAgg(column="won", aggfunc="count"), winrate=pd.NamedAgg(column="won", aggfunc="mean")).reset_index()
+        card_pair_stats = card_pair_group_by.aggregate(
+            games=pd.NamedAgg(column=GameData.WON_COLUMN_INDEX, aggfunc="count"), 
+            winrate=pd.NamedAgg(column=GameData.WON_COLUMN_INDEX, aggfunc="mean")).reset_index()
         card1_data = self._move_card_names_to_data(card_pair_stats, card1, card2)
         card2_data = self._move_card_names_to_data(card_pair_stats, card2, card1)
         return [card1_data, card2_data]
     
     def as_dataframe(self) -> pd.DataFrame:
         return self._data.copy(deep=True)
+    
+    def as_dict(self) -> dict:
+        cards = self._data["card1"].unique()
+        card_index_dict = {}
+        for card1 in cards:
+            card1_dict = {}
+            for card2 in cards:
+                if card1 == card2:
+                    continue
+                card_pair_data = self._data[(self._data["card1"] == card1) & (self._data["card2"] == card2)]
+                card1_dict[card2] = {
+                    "Both In Deck Games": card_pair_data["games"].sum(),
+                    "Neither Drawn Winrate": card_pair_data[(card_pair_data["card1_drawn"] == False) & (card_pair_data["card2_drawn"] == False)]["winrate"].mean(),
+                    "First Drawn Winrate": card_pair_data[(card_pair_data["card1_drawn"] == True) & (card_pair_data["card2_drawn"] == False)]["winrate"].mean(),
+                    "Second Drawn Winrate": card_pair_data[(card_pair_data["card1_drawn"] == False) & (card_pair_data["card2_drawn"] == True)]["winrate"].mean(),
+                    "Both Drawn Winrate": card_pair_data[(card_pair_data["card1_drawn"] == True) & (card_pair_data["card2_drawn"] == True)]["winrate"].mean(),
+                }
+            card_index_dict[card1] = card1_dict
+        return card_index_dict
+            
+
+            
+
